01c18f5c2584b35e9fe1212a820ace5e7c00b31b
dgmp <dgmp@fresnou.org>
2023-03-14 14:18:47 -0700

    functions1

---
+++ functions1.js
@@ -0,0 +1,46 @@
+// //Problem #1
+// function findAreaOfRectangle(length, width){
+//     console.log(`The area of the rectangle is ${length*width}`);
+// }
+// findAreaOfRectangle(7,4);
+
+// //Problem #2
+// function findAreaOfCircle(radius){
+//     console.log(`The area of the circle is ${3.14*radius*radius}`);
+// }
+// findAreaOfCircle(4);
+
+// //Problem #3
+// function findMin(x, y){
+//     if (x < y){
+//         console.log(`${x} is the smaller of the two.`);
+//     } else {
+//         console.log(`${y} is the smaller of the two.`)
+//     }
+// }
+// findMin(3, 20);
+
+// //Problem #4
+// function checkEquality(a, b){
+//     if (a == b){
+//         console.log(`${a} is equal to ${b}`);
+//     } else {
+//         console.log("The two numbers are not equal");
+//     }
+// }
+// checkEquality(3, 3);
+
+// //Problem #5
+// function caculateLargestRectangle(TriL1, TriW1, TriL2, TriW2){
+//     let triangle1Area = TriL1 * TriW1;
+//     let triangle2Area = TriL2 * TriW2;
+//     if(triangle1Area > triangle2Area){
+//         console.log("The first triangle has the larger area. ");
+//     } else {
+//         console.log("The second triangle has the larger area. ");
+//     }
+// }
+// caculateLargestRectangle(100, 3, 5, 10);
+
+//Problem 6
+function
