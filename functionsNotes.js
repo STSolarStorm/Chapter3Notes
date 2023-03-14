82c4861127d4cbfcb3ac7eeb0927bbe5640cf8c8
dgmp <dgmp@fresnou.org>
2023-03-14 13:54:05 -0700

    Initial Commit

--- functionsNotes.js
+++ functionsNotes.js
@@ -1,7 +1,40 @@
-const readLine = require('readline-sync');
-
 //Function Notes
 //Step 1 - Define the Function
-
+// function sayHello(){
+//     console.log("Hello");
+// }
 //Step 2 - Call (use) the Function
+// sayHello();
+// sayHello();
+// sayHello();
+// sayHello();
 
+
+// function printMax(){
+//     let x = 6;
+//     let y = 29;
+//     if (x > y){
+//         console.log(`The larger number is ${x}`);
+//     } else {
+//         console.log(`The larger number is ${y}`);
+//     }
+// }
+// printMax();
+
+// function printMax(x, y){
+//     if (x > y){
+//         console.log(`The larger number is ${x}`);
+//     } else {
+//         console.log(`The larger number is ${y}`);
+//     }
+// }
+// printMax(8,21);
+// printMax(7,-21);
+// printMax(2,2);
+// printMax(-3,21-20);
+
+function sum(number1, number2){
+    console.log(`${number1} + ${number2} = ${number1+number2}`);
+}
+sum(7, 12);
+sum(21, 15);
