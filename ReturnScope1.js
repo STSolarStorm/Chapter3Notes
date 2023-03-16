b87264cb3124ea01d2109d40d569c2d2bb781011
dgmp <dgmp@fresnou.org>
2023-03-16 14:18:18 -0700

    Return Scope

---
+++ ReturnScope1.js
@@ -0,0 +1,55 @@
+const readLine = require('readline-sync');
+
+// //Problem 1
+// function nameSwapper(){
+//     let first = readLine.question('What is your first name?: ');
+//     let last = readLine.question('What is your last name?: ');
+//     return (`${last}, ${first}`);
+// }
+//let myName = nameSwapper();
+// console.log(nameSwapper());
+
+// //Problem 2
+// function numberGenerator(){
+//     let integer1 = readLine.question('Enter an integer between 0 and 9: ');
+//     let integer2 = readLine.question('Enter another integer between 0 and 9: ');
+//     let total = integer1 + integer2;
+//
+//     return (total);
+// }
+// console.log('Your number is ' + numberGenerator());
+
+// //Problem 3
+// function averageTemp(temp1, temp2, temp3, temp4, temp5, temp6, temp7){
+//     let tempSum = temp1 + temp2 + temp3 + temp4 + temp5 + temp6 + temp7;
+//     let tempAve = tempSum / 7.;
+//     return (tempAve).toFixed(2);
+// }
+// console.log(averageTemp(20, 15, 50, 100, 42, 80, 89));
+
+
+
+
+
+// //Problem 4 NOT FINISHED YET!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Try using an 'If' or 'While' loop
+// function charityCollectionRevisted(){
+//     let peopleNumber = Number(readLine.question('How many people are raising money?: '));
+//     if (Money < 1000) {
+//         console.log(`$${Money} was raised, so $${Money + 100} will be donated.`);
+//     } else if (Money > 1000 && Money < 2000 ){
+//         console.log(`$${Money} was donated, so $${Money * 2} will be donated`);
+//     } else if(Money > 2000){
+//         console.log(`$${Money} was donated, so $${Money + 2000} will be donated.`)
+//     }
+// }
+
+
+
+
+
+
+
+//Problem 5
+function dartsGame(){
+
+}
