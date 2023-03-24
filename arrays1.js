b66d42bb60d68bbc36fb066dab16f02910c0624d
dgmp <dgmp@fresnou.org>
2023-03-24 14:18:09 -0700

    Working on Arrays

--- arrays1.js
+++ arrays1.js
@@ -33,3 +33,29 @@
 //         console.log(Array[i]);
 //
 //     }
+
+
+// //Problem 5
+// let Problem5Array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+// console.log(Problem5Array[2]);
+// console.log(Problem5Array[5]);
+// console.log(Problem5Array[8]);
+// console.log(Problem5Array[11]);
+//
+//
+// //Problem 6
+// let Problem6Array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+// console.log(Problem6Array[2]);
+// console.log(Problem6Array[4]);
+// console.log(Problem6Array[6]);
+// console.log(Problem6Array[8]);
+// console.log(Problem6Array[10]);
+// console.log(Problem6Array[12]);
+
+
+// //Problem 7 Working on it...
+// let Problem7Array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+// let prob7Arr =  Problem7Array.length - 11;
+// if(Problem7Array >= prob7Arr){
+//     console.log(`${Problem7Array}`);
+// }
