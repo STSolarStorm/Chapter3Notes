c776a1881d7ecc9f16addc7a09b902732c3616ed
dgmp <dgmp@fresnou.org>
2023-03-29 14:08:14 -0700

    finished 9

--- arrays2.js
+++ arrays2.js
@@ -68,25 +68,100 @@
 // replaceBig([8, 2, 3, 4, 5, 6]);
 
 
-//Problem 6
-function replaceBig(y){
-    let endVal = y.length;
-    let firstVal = y[0];
-    let newArr = [];
-    if (endVal > firstVal) {
-        for (let i = 0; i < y.length; i++) {
-            newArr.push(y[i]);
-            newArr[i] = endVal;
-        }
-        console.log(newArr);
-        console.log(y);
-    } else if (firstVal > endVal){
-        for (let i = 0; i < y.length; i++) {
-            newArr.push(y[i]);
-            newArr[i] = firstVal;
-        }
-        console.log(newArr);
-        console.log(y);
+// //Problem 6
+// function replaceBig2(y){
+//     let endVal = y.length;
+//     let firstVal = y[0];
+//     let newArr = [];
+//     if (endVal > firstVal) {
+//         for (let i = 0; i < y.length; i++) {
+//             newArr.push(y[i]);
+//             newArr[i] = endVal;
+//         }
+//         console.log(newArr);
+//         console.log(y);
+//     } else if (firstVal > endVal){
+//         for (let i = 0; i < y.length; i++) {
+//             newArr.push(y[i]);
+//             newArr[i] = firstVal;
+//         }
+//         console.log(newArr);
+//         console.log(y);
+//     }
+// }
+// replaceBig2([8, 2, 3, 4, 5, 6]);
+
+
+// //Problem 7
+// function replaceSmol(y){
+//     let endVal = y.length;
+//     let firstVal = y[0];
+//     let newArr = [];
+//     if (endVal < firstVal) {
+//         for (let i = 0; i < y.length; i++) {
+//             newArr.push(y[i]);
+//             newArr[i] = endVal;
+//         }
+//         console.log(newArr);
+//     } else if (firstVal < endVal){
+//         for (let i = 0; i < y.length; i++) {
+//             newArr.push(y[i]);
+//             newArr[i] = firstVal;
+//         }
+//         console.log(newArr);
+//     }
+// }
+// replaceSmol([8, 2, 3, 4, 5, 6]);
+//
+//
+// //Problem 8
+// function replaceSmol2(y){
+//     let endVal = y.length;
+//     let firstVal = y[0];
+//     let newArr = [];
+//     if (endVal < firstVal) {
+//         for (let i = 0; i < y.length; i++) {
+//             newArr.push(y[i]);
+//             newArr[i] = endVal;
+//         }
+//         console.log(newArr);
+//         console.log(y);
+//     } else if (firstVal < endVal){
+//         for (let i = 0; i < y.length; i++) {
+//             newArr.push(y[i]);
+//             newArr[i] = firstVal;
+//         }
+//         console.log(newArr);
+//         console.log(y);
+//     }
+// }
+// replaceSmol2([1, 2, 3, 4, 5, 6]);
+
+
+//Problem 9
+function ReplaceProductandDividend(number) {
+    let product = 1;
+    let arr9 = [];
+    for (let i = 0; i < number.length; i++) {
+        product = product * number[i];
     }
+    for (let i = 0; i < number.length; i++) {
+        arr9[i] = product/number[i];
+    }
+    console.log(arr9);
 }
-replaceBig([8, 2, 3, 4, 5, 6]);
+(ReplaceProductandDividend([1, 4, 6, 2, 4]));
+
+
+
+// function question9 (array9) {
+//     let newarray9 = [];
+//     let i = array9[0] * array9[1] * array9[2] * array9[3] * array9[4];
+//     for (let x = 0; x < array9.length; x++ ) {
+//         newarray9.push(i / array9[x])
+//     }
+//     console.log(newarray9)
+// }
+// question9 ([1, 2, 3, 4, 5]);
+
+//Problem 10
