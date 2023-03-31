ed8ef504c2fcfcb60a4ac8d2c38ab8889ad9544e
dgmp <dgmp@fresnou.org>
2023-03-31 14:19:39 -0700

    Madlibs

---
+++ Madlibs.js
@@ -0,0 +1,48 @@
+const readLine = require('readline-sync');
+
+
+
+let array = [adjective1, verb1, adjective2, noun1, verb2, verb3, adjective3, verb4, verb5, verb]
+let adjective1 = [readLine.question('Adjective: ')];
+let verb1 = readLine.question('Verb: ');
+let adjective2 = readLine.question('Adjective: ');
+let noun1 = readLine.question('Noun:');
+let verb2 = readLine.question('Verb: ');
+let verb3 = readLine.question('Verb: ');
+let adjective3 = readLine.question('Adjective: ');
+let verb4 = readLine.question('Verb: ');
+let verb5 = readLine.question('Verb: ');
+let noun2 = readLine.question('Noun; ');
+let verb6 = readLine.question('Verb: ');
+let adjective4 = readLine.question('Adjective: ');
+let verb7 = readLine.question('Verb: ');
+let adjective5 = readLine.question('Adjective: ');
+let noun3 = readLine.question('Noun: ');
+let adverb1 = readLine.question('Adverb:');
+let noun4 = readLine.question('Noun: ');
+let adjective6 = readLine.question('Adjective: ');
+let noun5 = readLine.question('Noun:');
+let verb8 = readLine.question('verb: ');
+let noun6 = readLine.question('Noun:');
+let noun7 = readLine.question('Noun:');
+
+let
+
+console.log(`Ronald wanted, no, needed to get that MacGuffin. He needed it so bad that if he didn’t get it, he’ll turn ${adjective1}.
+He ${verb1} his foot into the ground, with a determined mindset. Jumping into his ${adjective2} ${noun1}, he ${verb2} a switch and is ${verb3} out.
+It made him very ${adjective3}. Instead, he ${verb4} down the road to ${verb5} a window. into a house. Ronald steals a pencil from the ${noun2} and ${verb6} onto the road.
+He bends over to write on the ground that he is ${adjective4}. Suddenly, he gets ${verb7} in the head. He’s having a very ${adjective5} day. Ronald has had enough.
+He will get that MacGuffin no matter the cost. Using nothing but his ${noun3}, he walks ${adverb1} down the road once more. He powers through all the ${noun4} thrown
+at him and reaches the MacGuffin on the stump. When he grabs at it, a powerful shudder runs through his body. He’s finally done it! He finally has the almighty ${adjective6} ${noun5}.
+${verb8}, he turns around and points it at a ${noun6}. Then someone plucks it out of his hand and tosses it into the ${noun7}.
+`);
+}
+
+
+// Ronald wanted, no, needed to get that MacGuffin. He needed it so bad that if he didn’t get it, he’ll turn red.
+// He plants his foot into the ground, with a determined mindset. Jumping into his grey car, he flicks a switch and is launched out.
+// It made him very angry. Instead, he runs down the road to smash a window. into a house. Ronald steals a pencil from the table and dashes onto the road.
+// He bends over to write on the ground that he is peaceful. Suddenly, he gets kicked in the head. He’s having a very bad day. Ronald has had enough.
+// He will get that MacGuffin no matter the cost. Using nothing but his feet, he walks calmly down the road once more. He powers through all the rocks thrown
+// at him and reaches the MacGuffin on the stump. When he grabs at it, a powerful shudder runs through his body. He’s finally done it! He finally has the almighty golden stick.
+// Laughing, he turns around and points it at a bush. Then someone plucks it out of his hand and tosses it into the trashcan.
