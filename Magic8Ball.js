45ac72f5d5e94240a8ddb4335cfc989a1634f0d0
dgmp <dgmp@fresnou.org>
2023-04-14 14:16:53 -0700

    Magic8Ball Finished!!

--- Magic8Ball.js
+++ Magic8Ball.js
@@ -2,26 +2,27 @@ const readLine = require('readline-sync');
 
 let answers = ["Yes", "No", "Maybe", "It Is Decidedly So", "Absolutely", "Absolutely Not", "My Sources Say No", "Ask Again Later", "Prediction Unavailable", "Can't Tell You"];
 
-console.log("To use the program, enter in a question.");
+console.log('To use the program, enter in a question. To end the program, enter "End" or "Stop"');
 
-
-
-
-
-let userQuest = readLine.question("Question: ");
 let playTime = 0;
-    while (playTime < 8) {
-
+let userQuest = readLine.question("Question: ");
+    while (userQuest != "End" || userQuest == "Stop" || userQuest == "end" || userQuest == "stop") {
         if (userQuest == "") {
             console.log("User did not provide a question.");
-        }else {
+            userQuest = readLine.question("Question: ");
+        }else{
             let randomNum = Math.floor(Math.random() * answers.length);
             let answer = answers[randomNum];
             console.log(answer);
+            userQuest = readLine.question("Question: ");
         }
         playTime++;
-    }
-    console.log(`End of Program. You have run the program ${playTime} times.`);
+        if (playTime == 8){
+            console.log(`End of Program. You have run the program ${playTime} times.`);
+            break;
+        }
 
 }
-    console.log("End of Program");
+if (userQuest == "End" || userQuest == "Stop" || userQuest == "end" || userQuest == "stop"){
+    console.log(`End of Program. You have run the program ${playTime} times.`);
+}
