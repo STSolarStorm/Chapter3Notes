7c09b4f330f4671161aecbf8a5fd6e8b4823544e
dgmp <dgmp@fresnou.org>
2023-04-24 14:37:32 -0700

    Finished Iteration 4 and Next Steps of comboMenu

--- comboMenu.js
+++ comboMenu.js
@@ -13,6 +13,12 @@ let largeFries = 2.00;
 let sandwich;
 let beverage;
 let fries;
+let sandWich = 0;
+let bev = 0;
+let fry = 0;
+let finalSandwich = "";
+let finalBevSize = "";
+let finalFrySize = "";
 // let order = {
 //
 // };
@@ -20,15 +26,17 @@ let fries;
 //Iteration 1
 let askSandwich = readLine.question("What kind of sandwich do you want? Chicken ($5.25), Beef ($6.25), or Tofu ($5.75)?: ");
 if (askSandwich == "Chicken"){
-    console.log(`That Chicken Sandwich will cost you $${chickenSandwich}.`);
-    // askSandwich = chickenSandwich;
     sandwich = chickenSandwich;
+    finalSandwich = "Chicken";
+    sandWich++;
 } else if (askSandwich == "Beef"){
-    console.log(`That Beef Sandwich will cost you $${beefSandwich}.`);
     sandwich = beefSandwich;
+    finalSandwich = "Beef";
+    sandWich++;
 } else if (askSandwich == "Tofu"){
-    console.log(`That Tofu Sandwich will cost you $${tofuSandwich}.`);
     sandwich = tofuSandwich;
+    finalSandwich = "Tofu";
+    sandWich++;
 }
 
 
@@ -37,15 +45,21 @@ let askBeverage = readLine.question("Would you like to purchase a beverage?: ");
 if (askBeverage == "Yes"){
     let askBevSize = readLine.question("What beverage size would you like? Small ($1.00), Medium ($1.50), or Large ($2.25)?: ");
     if (askBevSize == "Small"){
-        console.log(`That small beverage will cost $${smallBev}.`);
         beverage = smallBev;
+        finalBevSize = "Small";
+        bev++;
+
     }else if (askBevSize == "Medium"){
-        console.log(`That medium beverage will cost $${mediumBev}.`);
         beverage = mediumBev;
+        finalBevSize = "Medium";
+        bev++;
     }else if (askBevSize == "Large"){
-        console.log(`That large beverage will cost $${largeBev}.`);
         beverage = largeBev;
+        finalBevSize = "Large";
+        bev++;
     }
+} else {
+    beverage = 0;
 }
 
 
@@ -57,23 +71,58 @@ if (askFries == "Yes"){
 
         let megaSize = readLine.question("Mega-sizing your fries will give you large fires, but bring the price to that of the large. Would you like to mega-size your fries?: ");
         if (megaSize == "Yes"){
-            console.log(`That'll cost you ${largeFries}`);
-            fries = largeFries
+            fries = largeFries;
+            finalFrySize = "Large";
+            fry++;
         }else if (megaSize == "No"){
-            console.log(`The small fries will cost $${smallFries}.`);
             fries = smallFries;
+            finalFrySize = "Small";
+            fry++;
         }
     }else if (askFriesSize == "Medium"){
-        console.log(`The medium fires will cost $${mediumFries}.`);
         fries = mediumFries;
+        finalFrySize = "Medium";
+        fry++;
     }else if (askFriesSize == "Large"){
-        console.log(`The large fries will cost $${largeFries}.`);
         fries = largeFries;
+        finalFrySize = "Large";
+        fry++;
     }
+} else {
+    fries = 0;
 }
 
 
+//Iteration 4
+let ketchupPacketRequest = Number(readLine.question("How many ketchup packets ($0.25 per packet) would you like?: "));
+let ketchupPacket = 0.25 * ketchupPacketRequest;
+let order = sandwich + beverage + fries + ketchupPacket;
+let taxedOrder = order * 0.07;
+let finalOrder = order + taxedOrder;
+if (sandWich == 1 && beverage == 1 && fries == 1){
+    let comboOrder = order - 1.00;
+    let taxedComboOrder = comboOrder * 0.07;
+    let finalComboOrder = comboOrder + taxedComboOrder;
+    console.log(`You ordered a ${finalSandwich} Sandwich.`);
+    if (askBeverage == "Yes"){
+        console.log(`You ordered a ${finalBevSize} Beverage`);
+    }
+    if (askFries == "Yes")
+        console.log(`You ordered ${finalFrySize} Fries`);
+    if (ketchupPacketRequest > 0){
+        console.log(`You ordered ${ketchupPacketRequest} Ketchup Packets. `);
+    }
+    console.log(`Your ordered a Combo!. The order comes to ${finalComboOrder.toFixed(2)}`);
+} else{
+    console.log(`You ordered a ${finalSandwich} Sandwich.`);
+    if (askBeverage == "Yes"){
+        console.log(`You ordered a $${finalBevSize} Beverage`);
+    }
+    if (askFries == "Yes")
+    console.log(`You ordered ${finalFrySize} Fries`);
+    if (ketchupPacketRequest > 0){
+        console.log(`You ordered ${ketchupPacketRequest} Ketchup Packets. `);
+    }
+    console.log(`Your order comes to $${finalOrder.toFixed(2)}`);
+}
 
-// let order = askSandwich + askBeverage + askFries;
-let order = sandwich + beverage + fries;
-console.log(order);
